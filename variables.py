29bc10d377ce92e60ae60b9cbfaf21264f6a5a9c
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-18 09:26:56 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+name = "Juan"
+print(name)
+
+num1 = 100
+num2 = 200
+
+print(num1 + num2)
+
+#python is case sensitive
+
+Book = "Mujeres"
+book = "Los miserables"
+print(Book == book)
+
+# another way
+
+x, dir = 32.52, "Carrera 2"
+
+print(dir,x)
+
+#Conventions
+book_name = "Cómo ser un estoico" # Snake case
+bookName = "El Enquiridión" # Camel case
+BookName = "Gargantua y pantagruel" # Pascal case
