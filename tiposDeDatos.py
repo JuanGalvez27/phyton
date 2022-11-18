bb479f2ef3890c228acbcd16ddc21bf273f5a065
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-18 09:12:29 -0500

    Data types

--- tiposDeDatos.py
+++ tiposDeDatos.py
@@ -1,9 +1,21 @@
 print("hello world")
 print("Hello World")
-
-print("bye" + "world") #str
-
-print(type(2.2145124513)) #float
-print(type(False)) #boolean
-
+#str
+print("bye" + "world") 
+#float
+print(type(2.2145124513)) 
+#boolean
+print(type(False)) 
 print(True)
+#list
+print( type([1,"Fernando", "god", 5.2345, False])) 
+#tuples
+(10,20,30,55) #lista inmutable
+print(type((10,23,40)))
+#dictionaries
+print(type({
+  "Ryan": "Ray",
+  "Juan": "galvez"
+}))
+#NonType
+print(type(None))
