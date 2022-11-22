881b10648677d92a7fb4d1ef97ac79fe272b12da
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-21 23:03:00 -0500

    Loops

---
+++ flowControl/conditionals.py
@@ -0,0 +1,9 @@
+x = 10
+
+if x > 10:
+  print("x es mayor que 10")
+elif x==10 :
+  print("x es 10")
+else:
+  print("x es menor que 10")
+  
---
+++ flowControl/loops.py
@@ -0,0 +1,19 @@
+foods = ["apples", "bread", "cheese", "milk"]
+
+for fruits in foods:
+  if fruits == "cheese":
+    print("foots smell")
+    break
+  print(fruits)
+
+
+for numbers in range(1,8):
+  print(numbers)
+
+for letter in "Carajo":
+  print(letter)
+
+count = 4
+while count <= 10:
+  print(count)
+  count = count + 1
